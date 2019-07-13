ebc5bf692d0a6fb828d052f2c34adccca489411c
JeongHeon Woo <email@woojeongheon.com>
2019-07-13 10:34:54 +0000

    add classify_data_in_date.py

--- namsigdang_crawler/namsigdang_crawler_2.0/classify_data_in_date.py
+++ namsigdang_crawler/namsigdang_crawler_2.0/classify_data_in_date.py
@@ -10,9 +10,3 @@ file_all_menu_dat_old = open(path_all_menu_dat, 'rb')
 dic_all_menu = pickle.load(file_all_menu_dat_old)
 file_all_menu_dat_old.close()
 
-print("{")
-
-for y in sorted(dic_all_menu):
-    print("\"" + y + "\": \"" + dic_all_menu[y] + "\",")
-
-print("}")
