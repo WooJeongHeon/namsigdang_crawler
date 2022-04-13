82c4494678afebc98862654863020d37b4a0e362
JeongHeon Woo <contact@wookingwoo.com>
2022-04-13 22:16:24 +0900

    Add permission: sudo

--- namsigdang_crawler/start_namsigdang_crawling.sh
+++ namsigdang_crawler/start_namsigdang_crawling.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 cd /home/ubuntu/namsigdang-server/namsigdang_crawler
-/usr/bin/python3 main.py
+sudo /usr/bin/python3 main.py
 
 # 0 */6 * * * /home/ubuntu/namsigdang-server/namsigdang_crawler/start_namsigdang_crawling.sh > /home/ubuntu/namsigdang-server/namsigdang_crawler/data/log/crontab_log/`date +\%Y\%m\%d`_`date +\%H\%M\%S`.log 2>&1
